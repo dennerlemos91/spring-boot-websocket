408ebc378771365e38dcbca7b20845b1d05a2664
Denner Lemos <dennerlemos91@gmail.com>
2020-02-21 01:15:45 -0300

    Initial commit

---
+++ src/main/java/br/com/dennerfl/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package br.com.dennerfl.controller;
+
+import br.com.dennerfl.model.Greeting;
+import br.com.dennerfl.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
---
+++ src/test/java/br/com/dennerfl/SpringBootWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dennerfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
